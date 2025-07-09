e7d798c061d05850f9eea075bfb23d6ea6cc8911
yuuto <yuuto@NCC>
2025-07-09 17:22:23 +0900

    4択に修正

--- Git_2025_Other2_Assignment/Quiz.js
+++ Git_2025_Other2_Assignment/Quiz.js
@@ -7,16 +7,21 @@ const quizData = {
 
 const questionElement = document.querySelector('.question');
 const choiceButtons = document.querySelectorAll('.choice-button');
+const resultElement = document.querySelector('.result');
+
 
 questionElement.textContent = quizData.question;
 choiceButtons.forEach((button, index) => {
     button.textContent = quizData.choices[index];
 
+
     button.addEventListener('click', () => {
         if (button.textContent === quizData.correct) {
-            alert("正解です！");
+            resultElement.textContent = "正解です！";
+            resultElement.style.color = "green";
         } else {
-            alert("不正解です。");
+            resultElement.textContent = "不正解です。";
+            resultElement.style.color = "red";
         }
     });
 });
